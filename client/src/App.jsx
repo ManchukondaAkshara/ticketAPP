ce6c945d29e0f1671184b7ca894b7a7d5fe18fb8
Akshara <23wh1a6613@bvrithyderabad.edu.in>
2025-07-07 22:21:46 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,12 @@ import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favourite from './pages/Favourite'
 import Footer from './components/Footer'
+import Layout from './pages/admin/Layout'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 import {Toaster} from 'react-hot-toast'
+import Dashboard from './pages/admin/Dashboard'
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
@@ -23,6 +28,12 @@ const App = () => {
         <Route path = '/movies/:id/:date' element={<SeatLayout/>} />
         <Route path="/my-bookings" element={<MyBookings/>}/>
         <Route path="/favourite" element={<Favourite />} />
+        <Route path = '/admin/*' element = {<Layout/>} >
+            <Route index element= {<Dashboard/>}/>
+            <Route path="add-shows" element={<AddShows />} />
+            <Route path="list-shows" element={<ListShows />} />
+            <Route path="list-bookings" element={<ListBookings />} />
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -3,6 +3,8 @@ import { dummyBookingData } from '../assets/assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import { useEffect } from 'react'
 import Loading from '../components/Loading'
+import timeFormat from '../lib/timeFormat'
+import { dateFormat } from '../lib/dateFormat'
 const MyBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY
   const [bookings, setBookings] = useState([])
@@ -24,9 +26,23 @@ const MyBookings = () => {
       {bookings.map((item,index) => (
         <div key = {index} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg p-2 mt-4 max-w-3xl'>
           <div className='flex flex-col md:flex-row'>
-            <img src = {""} alt = ""/>
-            
+            <img src = {item.show.movie.poster_path} alt = "" className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded'/>
+            <div className='flex flex-col p-4'>
+              <p className='text-lg font-semibold'>{item.show.movie.title}</p>
+              <p className='text-gray-400 text-sm'>{timeFormat(item.show.movie.runtime)}</p>
+              <p className='text-gray-400 text-sm mt-auto'>{dateFormat(item.show.showDateTime)}</p>
           </div>
+        </div>
+        <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
+          <div className='flex items-center gap-4'>
+            <p className='text-2xl font-semibold mb-3'>{currency}{item.amount}</p>
+            {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
+        </div>
+        <div className='text-sm'>
+          <p><span className='text-gray-400'>Total Tickets:</span>{item.bookedSeats.length}</p>
+          <p><span className='text-gray-400'>Seat Number:</span>{item.bookedSeats.join(", ")}</p>
+        </div>
+        </div>
         </div>
       ))}
 
